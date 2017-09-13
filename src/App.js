50cd2223ff5797c7c8e8a01c6a24943d28ceec49
nghianv-ibl <nghianv@blockchainlabs.asia>
2017-09-13 15:05:14 +0700

    Fix Blank Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { remote } from 'electron';
 
 class App extends Component {
-  constructor() {
-    super();
-    console.log(remote.require('electron'));
-  }
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		console.log(remote.require('electron'));
+	}
+	render() {
+		return (
+			<div className="App">
+				<div className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h2>Welcome to React</h2>
+				</div>
+				<p className="App-intro">To get started, edit <code>src/App.js</code> and save to reload.</p>
+			</div>
+		);
+	}
 }
 
 export default App;
